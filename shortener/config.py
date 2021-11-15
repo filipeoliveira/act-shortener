4fde7cd329bf3e8162a121280b7212af140f58ad
Filipe Oliveira <filipe.jtoliveira@gmail.com>
2021-11-15 20:07:25 -0300

    chore: some configurations

---
+++ shortener/config.py
@@ -0,0 +1,8 @@
+#todo FIX THIS MAPPING
+
+DEBUG = True
+REDIS_URL = '127.0.0.1' # enter redis url
+
+MSG_UNKNOWN_SHORT_URL = 'Unknown short URL'
+MSG_ID_REQ_PARAM_MISSING = 'id request param missing.'
+MSG_ALREADY_SHORT_URL = 'Already a short URL.'
